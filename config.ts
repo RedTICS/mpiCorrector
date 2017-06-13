875355d945bf40ae3f1353d6f43a3d5866d79815
hugo <hfernandez@neuquen.gov.ar>
2017-06-13 09:32:12 -0300

    refactor variables

--- config.ts
+++ config.ts
@@ -1,7 +1,9 @@
+import * as configPrivate from './config.private';
+
 /*SETTINGS*/
 export const apiConfig = {
-    host:'127.0.0.1',
-    port: 3002, /* Deberá cambiarse por el port 80 para producción */
+    host: configPrivate.host,
+    port: configPrivate.port,
     pathPacienteMpi: '/api/core/mpi/pacientes/mpi'
 };
 export const tipoAlgoritmoMatcheo = 'Levenshtein';
